bc0956e1573aaa1079356b237a865e244cf1b02c
“harshaV87” <harsha.87b@gmail.com>
2023-05-15 21:59:55 -0400

    Initial Commit

---
+++ CustomShapes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomShapes
+//
+//  Created by Venkata harsha Balla on 5/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomShapes/CustomShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesApp.swift
+//  CustomShapes
+//
+//  Created by Venkata harsha Balla on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
